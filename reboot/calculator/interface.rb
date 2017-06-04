a0c9a5690f53fe8a24bb10ed2302f9765ddbcb86
Lauren DeFrancesco <lauren209@live.com>
2017-06-04 12:33:08 +0200

    rails new

---
+++ reboot/calculator/interface.rb
@@ -0,0 +1,8 @@
+require_relative "calculator"
+
+p "what is your first number?"
+first_number = gets.chomp
+p "what is your second number?"
+second_number = gets.chomp
+p "choose an operator [+, -, x, /]"
+operator = gets.chomp
